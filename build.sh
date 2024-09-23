2d5d7bae84a9da3c6f00e946016c8afb84e3d567
jakjus <fazzebo@gmail.com>
2024-09-24 02:10:46 +0900

    set latest mysql and adminer versions

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ GOOS=linux GOARCH=arm go build -o build/hbparser-linux-arm
 GOOS=darwin GOARCH=amd64 go build -o build/hbparser-macos-amd64
 go build -o build/hbparser-macos-arm
 GOOS=windows GOARCH=amd64 go build -o build/hbparser-windows-amd64
-chmod build/hbparser* 755
+chmod 755 build/hbparser*
